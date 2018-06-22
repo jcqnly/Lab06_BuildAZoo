0a1818dd27a0f72965baed86a17fe8325fdfc06c
jcqnly <jcqnly@gmail.com>
2018-06-21 18:37:27 -0700

    add classes

---
+++ BuildAZoo/BuildAZoo/Classes/Lactating/Mammal/Mammal2Leg/Mammal2Leg.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildAZoo.Classes.Lactating.Mammal.Mammal2Leg
+{
+    class Mammal2Leg
+    {
+    }
+}
---
+++ BuildAZoo/BuildAZoo/Classes/Lactating/Mammal/Mammal4Leg/Cat.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildAZoo.Classes.Lactating.Mammal.Mammal4Leg
+{
+    class Cat
+    {
+    }
+}
